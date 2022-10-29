f7dd83a0a0d086d7b049e483482d4cee01030fb3
manthangadhia <manthan.gadhia@proton.me>
2022-10-29 15:34:40 +0200

    Version with fully implemented model saving but incomplete model loading (working on load_state_dict function to load saved params.

--- SequentialNetwork.py
+++ SequentialNetwork.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from collections import OrderedDict
 
 class SequentialNetwork(nn.Module):
     '''
@@ -27,7 +28,7 @@ class SequentialNetwork(nn.Module):
         None.
 
         '''
-        super().__init__()
+        super(SequentialNetwork, self).__init__()
         for name, module in zip(names, network_list):
             self.add_module(name, module)
     
@@ -52,10 +53,49 @@ class SequentialNetwork(nn.Module):
         return X
     
     def __str__(self):
+        # TODO: Create a string represenation of the current model instance.
         return "This is a print"
     
     def save(self, PATH):
+        """
+        Saves the state_dict (type OrderedDict) to the given path as a .pt
+        file. This file can later be loaded into another instantiated model.
+
+        Parameters
+        ----------
+        PATH : String
+            Directs where the torch.save() function saves the state_dict for 
+            the given model instance. This string contains folder/filename 
+            information, and new folders/files are created as necessary.
+
+        Returns
+        -------
+        None.
+
+        """
         torch.save(self.state_dict(), PATH)
+        
+    def load_state_dict(self, state_dict):
+        
+        if not isinstance(state_dict, OrderedDict):
+            raise TypeError("Expected state_dict to be dict-like, got {}."
+                            .format(type(state_dict)))
+        """
+        Get all layer names.
+        Get all layers!!!!
+        Pass (un)initialised param to layer.
+        """
+# =============================================================================
+#         keys = list() 
+#         layer = list()
+#         for key in state_dict.keys():
+#             keys.append(key)
+#             params.append(state_dict.get(key))
+#         
+#         self.names = keys
+#         self.network_list = params
+# =============================================================================
+        
     
     def train(self, X_train, y_train):
         pass
